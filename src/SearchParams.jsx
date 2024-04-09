ab748cd95762c772c6719400b849c980cdfe9dcb
Errabi Mohamed Amine <errabi.ami@gmail.com>
2024-04-09 16:12:44 +0000

    Adding tailwindcss

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -23,8 +23,9 @@ const SearchParams = () => {
   const [adoptedPet] = useContext(AdoptedPetContext);
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -37,19 +38,26 @@ const SearchParams = () => {
         }}
       >
         {adoptedPet ? (
-          <div className="pet image-container">
+          <div>
             <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
           </div>
         ) : null}
         <label htmlFor="location">
           Location
-          <input id="location" placeholder="Location" name="location" />
+          <input
+            type="text"
+            id="location"
+            placeholder="Location"
+            name="location"
+            className="mb-5 block w-60"
+          />
         </label>
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
             name="animal"
+            className="mb-5 block w-60"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -67,7 +75,12 @@ const SearchParams = () => {
         </label>
         <label htmlFor="breed">
           Breed
-          <select disabled={!breeds.length} id="breed" name="breed">
+          <select
+            className="mb-5 block w-60 disabled:opacity-50"
+            disabled={!breeds.length}
+            id="breed"
+            name="breed"
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -76,7 +89,9 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <button className="rounded border-none bg-orange-500 px-6 py-2 text-white hover:opacity-50">
+          Submit
+        </button>
       </form>
       <Results pets={pets} />;
     </div>
